b1d6c058174cde88e8fefc421c11e97497489a79
PartoTheEagle <asalhaytham@gmail.com>
2016-08-20 19:12:06 +0100

    Add variable play ground area

---
+++ variable-playground.js
@@ -0,0 +1,29 @@
+// var person = {
+// 	name: 'Alhaytham',
+// 	age: 22
+// };
+
+// function updatePerson(obj){
+// 	// obj = {
+// 	// 	name: 'Alhaytham',
+// 	// 	age: 29
+// 	// };
+// 	obj.age = 29; 
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+
+// Array Example
+
+
+var grades = [15, 99]; 
+
+function addGrades(grades){
+	grades = [15, 99, 77]; 
+	debugger;
+}
+
+addGrades(grades);
+console.log(grades);
