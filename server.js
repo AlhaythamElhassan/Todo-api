a4c6913840c1af856ef19984e5383e399af35f26
PartoTheEagle <asalhaytham@gmail.com>
2016-08-23 10:36:54 +0100

    Adding Validation to the models

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/', function(req, res) {
 	res.send('Todo API Root');
 });
 
-// GET /todos?completed=true&q=work
+// GET /todos?completed=false&q=work
 app.get('/todos', function(req, res) {
 	var query = req.query;
 	var where = {};
@@ -28,7 +28,7 @@ app.get('/todos', function(req, res) {
 	if (query.hasOwnProperty('q') && query.q.length > 0) {
 		where.description = {
 			$like: '%' + query.q + '%'
-		}
+		};
 	}
 
 	db.todo.findAll({
@@ -39,57 +39,58 @@ app.get('/todos', function(req, res) {
 		res.status(500).send();
 	});
 });
+
 // GET /todos/:id
 app.get('/todos/:id', function(req, res) {
-	var todoID = parseInt(req.params.id, 10);
-	db.todo.findById(todoID).then(function(todo) {
+	var todoId = parseInt(req.params.id, 10);
+
+	db.todo.findById(todoId).then(function(todo) {
 		if (!!todo) {
 			res.json(todo.toJSON());
 		} else {
 			res.status(404).send();
 		}
-
 	}, function(e) {
-		res.status(500).json(e);
+		res.status(500).send();
 	});
 });
 
 // POST /todos
 app.post('/todos', function(req, res) {
-	var body = _.pick(req.body, "description", "completed");
+	var body = _.pick(req.body, 'description', 'completed');
 
 	db.todo.create(body).then(function(todo) {
 		res.json(todo.toJSON());
-
 	}, function(e) {
 		res.status(400).json(e);
 	});
 });
-// DELETE /todos:id
+
+// DELETE /todos/:id
 app.delete('/todos/:id', function(req, res) {
-	var todoID = parseInt(req.params.id, 10);
+	var todoId = parseInt(req.params.id, 10);
+
 	db.todo.destroy({
 		where: {
-			id: todoID
+			id: todoId
 		}
 	}).then(function(rowsDeleted) {
 		if (rowsDeleted === 0) {
-			res.status(400).json({
+			res.status(404).json({
 				error: 'No todo with id'
 			});
 		} else {
 			res.status(204).send();
 		}
-	}, function(e) {
-		res.status(500).json({
-			"error": "no todo found with that id"
-		});
+	}, function() {
+		res.status(500).send();
 	});
 });
+
 // PUT /todos/:id
 app.put('/todos/:id', function(req, res) {
-	var todoID = parseInt(req.params.id, 10);
-	var body = _.pick(req.body, "description", "completed");
+	var todoId = parseInt(req.params.id, 10);
+	var body = _.pick(req.body, 'description', 'completed');
 	var attributes = {};
 
 	if (body.hasOwnProperty('completed')) {
@@ -100,7 +101,7 @@ app.put('/todos/:id', function(req, res) {
 		attributes.description = body.description;
 	}
 
-	db.todo.findById(todoID).then(function(todo) {
+	db.todo.findById(todoId).then(function(todo) {
 		if (todo) {
 			todo.update(attributes).then(function(todo) {
 				res.json(todo.toJSON());
