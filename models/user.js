b21525e6209e8fe92fcdc100a95db85cd056b9fd
PartoTheEagle <asalhaytham@gmail.com>
2016-08-23 16:04:28 +0100

    Add auth token generation to POST /users/login

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 var bcrypt = require('bcrypt');
 var _ = require('underscore');
+var cryptojs = require('crypto-js');
+var jwt = require('jsonwebtoken');
 
 module.exports = function(sequelize, DataTypes) {
 	var user = sequelize.define('user', {
@@ -68,6 +70,25 @@ module.exports = function(sequelize, DataTypes) {
 			toPublicJSON: function() {
 				var json = this.toJSON();
 				return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
+			},
+			generateToken: function(type) {
+				if (!_.isString(type)) {
+					return undefined;
+				}
+				console.log('woking .................02');
+				try {
+					var stringDate = JSON.stringify({
+						id: this.get('id'),
+						type: type
+					});
+					var encryptedDate = cryptojs.AES.encrypt(stringDate, 'abc123!@#!').toString();
+					var token = jwt.sign({
+						token: encryptedDate
+					}, 'qwerty098');
+					return token;
+				} catch (e) {
+					return undefined;
+				}
 			}
 		}
 	});
